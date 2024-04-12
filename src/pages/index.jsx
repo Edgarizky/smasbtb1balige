83e6ea3e9a10b96d5e05429261fe228a4e348a58
edgar112-web <72640520+edgar112-web@users.noreply.github.com>
2024-04-12 16:11:48 +0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ import Wrapper from "../layout/wrapper";
 const Home = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={""} />
+      <SEO pageTitle={"SMAS Bintang Timur 1 Balige"} />
       <HomeOne />
     </Wrapper>
   );
